58c97b836b1407f8a67a33d3f323363ab80956dd
Andre Carrilho <andrecarrilho@gmail.com>
2010-08-20 22:17:52 +0100

    Added support for FluentHttp and some improvements

---
+++ TestWebRequest/SampleObject.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Runtime.Serialization;
+
+namespace TestWebRequest
+{
+    [DataContract(Namespace = "", Name = "sampleobject")]
+    public class SampleObject
+    {
+        [DataMember(Name = "id")]
+        public Guid Id { get; set; }
+
+        [DataMember(Name = "name")]
+        public string Name { get; set; }
+
+        [DataMember(Name = "subobject")]
+        public SubObject AnotherObject { get; set; }
+
+        [DataContract(Namespace = "", Name = "subobject")]
+        public class SubObject
+        {
+             [DataMember(Name = "age")]
+             public int Age { get; set; }
+
+            [DataMember(Name = "birthdate")]
+            public DateTime Birthdate { get; set; }
+        }
+    }
+}

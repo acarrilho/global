f7438434bca4841a057e68930c118d0bf6bca51e
Andre Carrilho <andre.carrilho@cycloid.pt>
2011-10-20 15:04:23 +0100

    made minor modifications

--- TestWebRequest/Program.cs
+++ TestWebRequest/Program.cs
@@ -1,13 +1,12 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.IO;
-using System.Security.Cryptography;
+using System.Runtime.Serialization;
 using System.Text;
 using System.Web;
-using Global;
+using System.Xml.Serialization;
 using Global.Global;
 using Global.Http;
-using Global.IO;
 using Global.Security;
 using Global.Xml;
 
@@ -17,52 +16,20 @@ namespace TestWebRequest
     {
         static void Main(string[] args)
         {
-            const string dataFmt = "{0,-30}{1}";
-            const string timeFmt = "{0,-30}{1:yyyy-MM-dd HH:mm}";
-
-            DateTime currentDate = DateTime.Now;
-            int currentYear = currentDate.Year;
-
-            TimeZone localZone = TimeZone.CurrentTimeZone;
-            DateTime currentUTC =
-            localZone.ToUniversalTime(currentDate);
-            TimeSpan currentOffset =
-                localZone.GetUtcOffset(currentDate);
-
-            Console.WriteLine(timeFmt, "Coordinated Universal Time:",
-                currentUTC);
-            Console.WriteLine(dataFmt, "UTC offset:", currentOffset);
-
-            //Console.WriteLine("Welcome. Don't know how to use? Type gt -h to see how!");
-
-            // 634294368000000000
-            // 1304527164696 1304602487502
-
-            //var ts = DateTime.UtcNow - new DateTime(1970, 1, 1);
-            //Console.WriteLine(ts.TotalMilliseconds);
-
-            //var init = new DateTime(1970, 1, 1, 0, 0 ,0);
-            //var fromNuno = init.AddMilliseconds(1304614396812);
-            //Console.WriteLine(fromNuno);
-
-            //Console.WriteLine(DateTime.MinValue);
-            //Console.WriteLine(new DateTime(1304602487502));
-            //Console.WriteLine(DateTime.MaxValue);
-
-            //var installationId = "8b7d8f719ebb411098f27b28073ef5fe"; // Guid.NewGuid().ToString().Replace("-", "");
-
-            //var c = new CryptoHelper {SecretKeyString = installationId};
-            //var str = "1:2:3:4"; // "1324lkdjgfldjhg908:91000000:" + installationId;
-            //var hashed = c.Encrypt(str, 128, 128);
+            //var str =
+            //    @"+mQHiA/xd3Uk6EJ5+37KnkfkjhcZM8gRd/mg04LhN9zQwoqiWgEhBnlS3+oktWDN9fXuo8HDk4stsBKL3lT6qkyhqPyomrB/8Nw6jMV5a7QuHs0e1GcM/8ob7GRGSdP+/YwIhrp6BeEeDZ9J3D0q8ZnTOlDmJQ3xpzqoajDb3Aq3283oaCKM0IQCIWWSPIlB6kz/SMS91Al9VixdW4kJbJfIU9BNV4w4uFHd1UXJSyuSA6ty5em/1lsBqr7jI90v";
+            //var c = new CryptoHelper { SecretKeyString = "6ea6db577fb840f8b516a65406eb58a9" };
+            //var hashed = c.Decrypt(str, 128, 128);
 
             //Console.WriteLine(hashed);
 
-            //var d = new CryptoHelper { SecretKeyString = "8b7d8f719ebb411098f27b28073ef5fe" /*installationId*/ };
-            //var dehashed = d.Decrypt("K1nlwL4J4X9dQzlSXRnjTg==" /*hashed*/, 128, 128);
+            var utcDateNow = DateTime.Now.ToUniversalTime();
+            var startDate = new DateTime(utcDateNow.Year, utcDateNow.Month, utcDateNow.Day, 0, 0, 0).AddHours(1).AddDays(0).Ticks;
+            var endDate = new DateTime(utcDateNow.Year, utcDateNow.Month, utcDateNow.Day, 23, 59, 59).AddHours(1).AddDays(0).Ticks;
 
-            //Console.WriteLine(dehashed);
-
-            //Console.WriteLine("K1nlwL4J4X9dQzlSXRnjTg==");
+            Console.WriteLine("{0} - {1}", startDate, endDate);
+            Console.WriteLine("{0} - {1}", new DateTime(startDate), new DateTime(endDate));
+            Console.WriteLine("{0} - {1}", new DateTime(startDate).AddHours(-1), new DateTime(endDate).AddHours(-1));
 
             //var program = new Program();
             //HashSomething();
@@ -71,8 +38,22 @@ namespace TestWebRequest
             //SendMail();
             //DataContractJsonSerialization();
 
-
             Console.ReadLine();
+        }
+
+        [DataContract(Namespace = "", Name="person")]
+        [XmlRoot(Namespace = "", ElementName="person")]
+        public class Person
+        {
+            [DataMember(Name="name")]
+            [XmlElement(ElementName = "name")]
+            public string Name { get; set; }
+            [DataMember(Name = "age")]
+            [XmlElement(ElementName = "age")]
+            public int Age { get; set; }
+            [DataMember(Name = "birth")]
+            [XmlElement(ElementName = "birth")]
+            public DateTime Birth { get; set; }
         }
 
         private static void HashSomething()
@@ -135,6 +116,7 @@ namespace TestWebRequest
             string site;
             string logPath;
             string exit = String.Empty;
+            var method = "";
 
             while (!String.Equals(exit, "exit"))
             {
@@ -147,19 +129,14 @@ namespace TestWebRequest
 
                     var http = new HttpHelper();
 
-                    var loop = false;
                     string postData = null;
-                    while(loop)
-                    {
-                        Console.WriteLine("How do you want to send the request [ GET | POST ]: ");
-                        var method = Console.ReadLine();
-                        loop = string.IsNullOrEmpty(method);
-                    }
+                    Console.WriteLine("How do you want to send the request [ GET | POST ]: ");
+                    method = Console.ReadLine();
 
                     var addMore = false;
                     Console.Write("Do you wish to add header values [ y | n ]: ");
                     var addHeaders = Console.ReadLine();
-                    if(!string.IsNullOrEmpty(addHeaders) || addHeaders.ToLower().Equals("y")) addMore = true;
+                    addMore = !string.IsNullOrEmpty(addHeaders) && addHeaders.ToLower().Equals("y");
                     while (addMore)
                     {
                         Console.Write("Key name: ");
@@ -176,8 +153,8 @@ namespace TestWebRequest
                         addMore = bool.Parse(Console.ReadLine());
                     }
 
-                    string content = http.SendRequest(site, postData, EnumHelper.StringToEnum<HttpRequestType>(""), null, null,
-                        true, null, null, null, Encoding.UTF8);
+                    var content = http.SendRequest(site, postData, EnumHelper.StringToEnum<HttpRequestType>(method), 
+                        null, null, true, null, null, null, Encoding.UTF8);
 
                     using (TextWriter txt = new StreamWriter(logPath))
                         txt.WriteLine(content);

ff8a629cc882cd3d52b7e3fd85764e00b8ab5754
Andre Carrilho <andre.carrilho@cycloid.pt>
2013-02-18 17:39:15 +0000

    added more extension methods to Common.cs

--- Global.DLL/Common.cs
+++ Global.DLL/Common.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
 using System.Text;
 
 namespace Global
@@ -72,6 +74,51 @@ namespace Global
         public static T StringToEnum<T>(string value, bool ignoreCase)
         {
             return (T)Enum.Parse(typeof(T), value, ignoreCase);
+        }
+
+        /// <summary>
+        /// Verifies if the list if null or empty.
+        /// </summary>
+        /// <typeparam name="TDocument">The generic time document.</typeparam>
+        /// <param name="list">The list to validate.</param>
+        /// <returns>A boolean indicating if the list is valid or not.</returns>
+        public static bool IsNullOrEmpty<TDocument>(this IEnumerable<TDocument> list)
+        {
+            return list == null || list.Count() <= 0;
+        }
+        /// <summary>
+        /// Converts from unix time to C#'s DateTime format.
+        /// </summary>
+        /// <param name="unixTime">The unix time.</param>
+        /// <returns>A DateTime representation of the unix time.</returns>
+        public static DateTime FromUnixTime(this long unixTime)
+        {
+            var epoch = new DateTime(1970, 1, 1, 0, 0, 0, DateTimeKind.Utc);
+            return epoch.AddSeconds(unixTime);
+        }
+        /// <summary>
+        /// Converts from C#'s DateTime format to unix time.
+        /// </summary>
+        /// <param name="date">The DateTime in C# format.</param>
+        /// <returns>A long with the converted unix time.</returns>
+        public static long ToUnixTime(this DateTime date)
+        {
+            var epoch = new DateTime(1970, 1, 1, 0, 0, 0, DateTimeKind.Utc);
+            return Convert.ToInt64((date - epoch).TotalSeconds);
+        }
+        /// <summary>
+        /// Adds functionality to insert a skip and take operations on the returned queryable list.
+        /// </summary>
+        /// <typeparam name="TDocument">The type of document.</typeparam>
+        /// <param name="queryable">The current queryable object.</param>
+        /// <param name="skip">The skip value.</param>
+        /// <param name="take">The take value.</param>
+        /// <returns>A list of the document type object.</returns>
+        public static List<TDocument> ToList<TDocument>(this IQueryable<TDocument> queryable, int skip, int take)
+        {
+            if (skip > 0) queryable = queryable.Skip(skip);
+            if (take > 0) queryable = queryable.Take(take);
+            return queryable.ToList();
         }
     }
 }
